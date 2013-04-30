2f3cbd1467212c5ecfa0208c4efdfa27d576878d
Christopher Roche <christopherroche@stmarksschool.org>
2013-04-30 19:23:13 -0400

    Beginning Version of a Hello World

---
+++ JavaGroupGameProject2013/src/JavaGroupGameProject2013.java
@@ -0,0 +1,12 @@
+
+public class JavaGroupGameProject2013 {
+
+	/**
+	 * This is the final Project for StMarks School Java
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hi from Java Group Project");
+	}
+
+}
