a9999c253aec69b3a890f3af1545584a62697a36
Christopher Roche <christopherroche@stmarksschool.org>
2013-04-30 20:33:06 -0400

    Adding the Frame to the Main()

--- JavaGroupGameProject2013/src/JavaGroupGameProject2013.java
+++ JavaGroupGameProject2013/src/JavaGroupGameProject2013.java
@@ -1,12 +1,19 @@
+import javax.swing.JFrame;
+
 
 public class JavaGroupGameProject2013 {
 
-	/**
-	 * This is the final Project for StMarks School Java
-	 */
+	static JFrame f;
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hi from Java Group Project");
+		
+		f = new JFrame("The Game with No Name");
+		f.setSize(700, 700);
+		
+		f.setVisible(true);
+		
 	}
 
 }
